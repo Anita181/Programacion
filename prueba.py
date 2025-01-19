4f1a59ee8452a026b4110c158aba09738a0cceea
Anais <anaisdelgado64@gmail.com>
2025-01-19 15:18:19 -0500

    commit inicial

---
+++ prueba.py
@@ -0,0 +1,30 @@
+#Este es mi primer programa, voy a escribir Hello World en la pantalla
+#print("Hello World")
+
+#Quiero pedirle una palabra al ausuario y mostrarla en pantalla
+#palabra = input("Dime una palabra: ")
+#print(palabra)
+
+#numero = input("Dime un numero: ")
+#numero=int(numero)
+#resultado = numero*2
+#print(resultado)   
+
+#if (resultado>100):
+#   print("mucho")
+#else:
+#   print("poco")
+
+
+
+for numero in range(0, 100):
+    print(f"numero: {numero}")
+    if (numero>50):
+        print("mucho")
+    else:
+        print("poco")
+
+
+
+#quiero pedirle al usuario dia, mes y año y mostrarlo en pantalla
+
